870370feb302d28e83913c6e3d7197fab8b83b9f
Alvaro-Hernandez <alvaropineda606@gmail.com>
2022-10-06 11:44:46 -0600

    Cunsumo de la Api, Funcion aninima o Funcion de flecha

--- src/Views/PokedexScreen.js
+++ src/Views/PokedexScreen.js
@@ -1,10 +1,39 @@
-import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, Text } from "react-native";
+import { getPokemonsApi } from "../Api/Pokemos";
 
 export default function PokedexScreen() {
+
+  useEffect(() => {
+    //Aqui crearemos una funcion anonima del pokemon con la siguiente instancia
+    //(async () => {
+    //  await loadPokemons();
+    //})();
+    const loadPokemons = async() => {
+      try {
+        const response = await getPokemonsApi();
+        console.log(response);
+      }
+      catch(error){
+        console.error(error);
+      }
+    }
+    return loadPokemons();
+  }, []);
+
+  //Esto es cuando usamos una funcion anonima el cual ocupara la respuesta de la api
+  /*const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };*/
+
   return (
     <SafeAreaView>
       <Text>PokedexScreen</Text>
     </SafeAreaView>
-  )
+  );
 }
